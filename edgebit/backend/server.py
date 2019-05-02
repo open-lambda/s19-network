ee043507b8c67d8c233c0643cbdd6620feb59228
omjb13 <omjb.13@gmail.com>
2019-05-01 21:53:29 -0600

    io workload

--- edgebit/backend/server.py
+++ edgebit/backend/server.py
@@ -3,8 +3,7 @@ import hashlib
 import logging
 import sys
 import os
-
-import requests
+import uuid
 
 this_src_dir = os.path.dirname(os.path.abspath(__file__))
 edgesim_package = os.path.abspath(os.path.join(this_src_dir, '../../'))
@@ -13,6 +12,9 @@ sys.path = [edgesim_package] + sys.path
 from edgesim.utils.server import run_server
 
 class EdgebitServer(object):
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+
     def foo(self, data, rounds):
         rounds = int(rounds)
         assert rounds > 0, "rounds has to be > 0"
@@ -24,10 +26,18 @@ class EdgebitServer(object):
             result = hashlib.sha256(data)
 
         return result.hexdigest()
+    
+    def bar(self, data):
+        fname = 'bar_%s' % uuid.uuid1().hex
+        with open(os.path.join(self.data_dir, fname), "w") as f:
+            f.write(data)
+            f.flush()
+            os.fsync(f.fileno())
 
     def get_http_endpoints(self):
         return {
             "foo": self.foo,
+            "bar": self.bar
         }
 
 def main():
@@ -35,9 +45,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", required=True)
     parser.add_argument("--port", type=int, default=8090)
+    parser.add_argument("--data-dir", default="/tmp")
+
     args = parser.parse_args()
 
-    run_server(args.host, args.port, EdgebitServer())
+    run_server(args.host, args.port, EdgebitServer(args.data_dir), threaded=True)
 
 if __name__ == '__main__':
     main()
